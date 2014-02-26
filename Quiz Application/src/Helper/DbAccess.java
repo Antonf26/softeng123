d5c7c71841070972c3b868fd810a41ef0f7ff74e
Antonf26 <antonf26@gmail.com>
2014-02-26 22:07:10 +0000

    created sample db, beginning of data access class

---
+++ Quiz Application/src/Helper/DbAccess.java
@@ -0,0 +1,66 @@
+package Helper;
+
+
+import java.sql.*;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Anton
+ */
+public class DbAccess {
+ 
+    public static  void getConnection()
+    {
+try
+{
+    
+
+        Class.forName("org.apache.derby.jdbc.ClientDriver");
+        con = DriverManager.getConnection("jdbc:derby://localhost:1527/quizAppDb", "test", "test");
+}
+catch (ClassNotFoundException ce)
+{
+    System.out.println("Can't find derby driver");
+}
+catch (SQLException se)
+{
+    System.out.println(se.getMessage());
+}
+
+        
+    }
+    public static int GetUser(String username, String password){
+    try
+    {
+        
+        String query = String.format("SELECT * FROM APPUSERS WHERE USERNAME = '%s' AND PASSWORD = '%s'", username, password);
+        
+        int utype = 0;
+        ResultSet rs = getQueryResults(query);
+        while (rs.next())
+                {
+                    utype = rs.getInt("USERTYPE");
+                }
+        return utype;
+    }
+    catch (SQLException se)
+            {
+            return -1;
+            }
+}
+    private static ResultSet getQueryResults(String query) throws SQLException
+    {
+        getConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        return rs;
+    }
+    public static Connection con;
+}
--- Quiz Application/src/Question.java
+++ Quiz Application/src/Question.java
@@ -1,3 +1,6 @@
+
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,29 @@
  * @author Anton
  */
 public class Question {
-    
+ private String QuestionText;
+ private List<String> Answers;
+ private int correctAnswerIndex;
+ private String Explanation;
+ public Boolean Validated;
+ //public Subject Subject;
+ public Question(String QuestionText, List<String> Answers, int correctAnswerIndex)
+ {
+     
+ }
+ public void ValidateQuestion()
+ {
+     
+ }
+ public void RejectQuestion()
+ {
+     
+ }
+ public void EditQuesion()
+ {
+     
+ }
+ 
+ 
+ 
 }
