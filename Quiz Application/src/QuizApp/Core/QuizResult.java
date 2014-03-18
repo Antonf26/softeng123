b97081caf21877eb6d13d671c5893d00df619b6d
Antonf26 <antonf26@gmail.com>
2014-03-18 09:05:52 +0000

    tweaks, adding login to the main gui

--- Quiz Application/src/QuizApp/Core/QuizResult.java
+++ Quiz Application/src/QuizApp/Core/QuizResult.java
@@ -20,4 +20,5 @@ public class QuizResult {
     public List<QuizResultRow> ResultRows;
     
     
+    
 }
---
+++ Quiz Application/src/QuizApp/Core/QuizResultRow.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package QuizApp.Core;
+
+/**
+ *
+ * @author Anton
+ */
+public class QuizResultRow {
+    
+    public QuizResultRow ()
+    {
+        
+    }
+    
+    
+    public String questionText;
+    public String correctAnswerText;
+    public String selectedAnswerText;
+    public boolean isCorrect;
+}
